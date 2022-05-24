3aca1e8f740c9871c40eb30c8ae87269f8355daf
drEdisn <denizs00@mail.ru>
2022-05-24 19:08:57 +0300

    feat: add ended phrase

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const { stdin } = require('process');
+const { stdin, stdout } = require('process');
 
 fs.writeFile(
   path.join(__dirname, 'text.txt'),
@@ -12,16 +12,18 @@ fs.writeFile(
 );
  
 let read = readline.createInterface({
-  input: stdin
+  input: stdin,
+  output: stdout
 });
 
 console.log('Write text here: ');
 
 read.on('line', (input) => {
   if (input === 'exit') {
-    console.log('Exit complete');
-    process.exit();
+    read.close();
   } else {
     fs.appendFile(path.join(__dirname, 'text.txt'), `${input}\n`, err => {if (err) throw err;});
   }
 });
+
+read.on('close', () => console.log('Exit complete'));
